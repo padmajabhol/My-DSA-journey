eea90c244f86dabd84155882ea5ff91f473d0182
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2021-08-10 23:48:05 +0530

    C++

---
+++ C and C++ concepts/C++/tut2.cpp
@@ -0,0 +1,18 @@
+ //variables
+ //int, float, char, double, boolean
+
+ #include<iostream>
+
+ using namespace std;
+
+ int main(){
+    //  int a = 4;
+    //  int b = 5;
+    int a=14, b=15;
+    float pi=3.14;
+    char c = 'u';//dont write more than one character
+     cout<<"This is tutorial 4.\n Here the value of a is "<<a<<"\n The value of b is " << b;
+     cout<<"\nThe value of pi is: "<<pi;
+     cout<<"\nThe value of c is: "<<c;
+     return 0;
+ }
