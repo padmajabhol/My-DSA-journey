d23d5cf1d60078e429d6aee529042dc3ec8b7794
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2021-08-21 22:55:59 +0530

    structures, unions and enums.

--- C and C++ concepts/C++/tempCodeRunnerFile.cpp
+++ C and C++ concepts/C++/tempCodeRunnerFile.cpp
@@ -1,11 +1,1 @@
-    // cout<<marks[0]<<endl;
-    // cout<<marks[1]<<endl;
-    // marks[2] = 90;
-    // cout<<marks[2]<<endl;
-    // cout<<marks[3]<<endl;
-
-    // for (int i = 0; i < 4; i++)
-    // {
-       
-    //     cout<<"The value of marks "<<i<<"  is "<<marks[i]<<endl;  
-    // }
+  // cout<<m1; //0
